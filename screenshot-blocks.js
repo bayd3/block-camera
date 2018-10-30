acacba6807c26a38a046ade54ce0fe3618151756
Micah Stubbs <micah.stubbs@gmail.com>
2018-10-29 20:12:21 -0700

    attempt well formed JSON POST

--- screenshot-blocks.js
+++ screenshot-blocks.js
@@ -1,19 +1,30 @@
 const fs = require('fs')
+const util = require('util')
 const d3 = require('d3-dsv')
 const screenshotBlock = require('./screenshot-block.js')
+const fetch = require('node-fetch')
+const URL = require('url').URL
 
-const path = './data/no-thumb.csv'
-const data = d3.csvParse(fs.readFileSync(path, 'utf-8'))
+const screenshotBlocks = async () => {
+  const path = `${__dirname}/data/no-thumb.csv`
+  const readFile = util.promisify(fs.readFile)
+  readFile(path, 'utf-8')
+    .then(file => d3.csvParse(file))
+    .then(data => {
+      // console.log('data', data)
+      // console.log('data[0]', data[0])
 
-// console.log('data[0]', data[0])
+      const subset = data.slice(data.length - 10, data.length)
+      console.log('subset', subset)
 
-data.some((block, i) => {
-  console.log(`${i} of ${data.length}`)
-  screenshotBlock({
-    block,
-    type: 'thumbnail'
-  })
-  if (i > 10) return true
-})
+      const screenshotServerUrl = new URL('https://screenshot.micah.fyi/api')
+      fetch(screenshotServerUrl, {
+        method: 'POST',
+        body: JSON.stringify(subset),
+        headers: { 'Content-Type': 'application/json' }
+      }).then(res => console.log(res))
+      // .then(json => console.log(json))
+    })
+}
 
-
+screenshotBlocks()
